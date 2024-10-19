e6c11aa2efd92d5f91a882e765678fd63fc628ca
sm <sahm1383@gmail.com>
2024-10-19 09:47:01 +0330

    fix admin

--- karbridge_app/admin.py
+++ karbridge_app/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
 from .models import (
-    KarfarmaProfile,
+    Karfarma,
     JobPosting,
     Product,
-    KarjoProfile,
+    Karjo,
     Resume,
-    Ticket,
-    OrganizationProfile
+    Organization,
+    Job_skill,
+    Benefits_And_Facilities,
+    Educational_Level,
+    Organization_Name,
 )
 
 # ثبت مدل‌ها در ادمین
-admin.site.register(KarfarmaProfile)
+admin.site.register(Karfarma)
 admin.site.register(JobPosting)
 admin.site.register(Product)
-admin.site.register(KarjoProfile)
+admin.site.register(Karjo)
 admin.site.register(Resume)
-admin.site.register(Ticket)
-admin.site.register(OrganizationProfile)
+admin.site.register(Organization)
+admin.site.register(Job_skill)
+admin.site.register(Benefits_And_Facilities)
+admin.site.register(Educational_Level)
+admin.site.register(Organization_Name)
