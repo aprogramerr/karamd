7a776bc41194593a72abe4d072500002d167c955
sm <sahm1383@gmail.com>
2024-10-19 09:47:40 +0330

    fix permmissions

--- karbridge_app/permissions.py
+++ karbridge_app/permissions.py
@@ -1,9 +1,23 @@
 from rest_framework import permissions
 
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+class IsKarfarma(permissions.BasePermission):
+    """
+    مجوز برای کارفرما
+    """
 
-class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user or request.user.role == 'admin'
+        return request.user == obj.karfarma.user
+
+    def has_permission(self, request, view):
+        return request.user.role == 'karfarma'
+
+class IsKarjo(permissions.BasePermission):
+    """
+    مجوز برای کارجو
+    """
+
+    def has_permission(self, request, view):
+        return request.user.role == 'karjo'
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.karjo.user
