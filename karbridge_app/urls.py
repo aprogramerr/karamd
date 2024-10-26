a6ebe3c68c49e7225b9240bd75a4c789856cc7c0
sm <sahm1383@gmail.com>
2024-10-26 13:37:47 +0330

    sowager & redoc

--- karbridge_app/urls.py
+++ karbridge_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import KarfarmaViewSet, OrganizationViewSet, JobPostingViewSet, JobSkillViewSet, BenefitsAndFacilitiesViewSet, ProductViewSet, KarjoViewSet, ResumeViewSet, EducationalLevelViewSet, OrganizationNameViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'karfarma', KarfarmaViewSet)
@@ -16,4 +20,21 @@ router.register(r'organizationname', OrganizationNameViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="عنوان API شما",
+        default_version="v1",
+        description="توضیحات API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    # آدرس Swagger UI
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    # آدرس Redoc UI
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
