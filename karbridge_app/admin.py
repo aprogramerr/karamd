0ed2e663d7c5022b2bcad86937c789337b88663e
mmreza <1tapciran@gmail.com>
2024-11-09 11:51:18 +0330

    Register JobCategory in django admin

--- karbridge_app/admin.py
+++ karbridge_app/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import (
     Karfarma,
     JobPosting,
+    Job_skill,
+    JobCategory,
     Product,
     Karjo,
     Resume,
     Organization,
-    Job_skill,
     Benefits_And_Facilities,
     Educational_Level,
     Organization_Name,
@@ -15,11 +16,12 @@ from .models import (
 # ثبت مدل‌ها در ادمین
 admin.site.register(Karfarma)
 admin.site.register(JobPosting)
+admin.site.register(Job_skill)
+admin.site.register(JobCategory)
 admin.site.register(Product)
 admin.site.register(Karjo)
 admin.site.register(Resume)
 admin.site.register(Organization)
-admin.site.register(Job_skill)
 admin.site.register(Benefits_And_Facilities)
 admin.site.register(Educational_Level)
 admin.site.register(Organization_Name)
